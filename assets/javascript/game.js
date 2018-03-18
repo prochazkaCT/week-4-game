aea45815a7cd79706ac44905f5c9a4796c783d57
prochazkaCT <sharon@countertools.org>
2018-03-18 14:59:10 -0400

    from home

---
+++ assets/javascript/game.js
@@ -0,0 +1,16 @@
+
+var snowflakes = $(".snowflakes");
+
+var randomNumber = 0;
+var wins = 0;
+var losses = 0;
+var snowOptions = [];
+
+for (var i = 0; i < 4; i++) {
+
+  var random = Math.floor(Math.random() * 15);
+  console.log(random);
+  
+
+
+}
